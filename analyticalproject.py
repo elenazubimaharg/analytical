7761594b856875dc1baa540f6b04bf1d2a59f57c
elenazubimaharg <145853111+elenazubimaharg@users.noreply.github.com>
2023-12-15 12:43:59 -0500

    Update analyticalproject.py

--- analyticalproject.py
+++ analyticalproject.py
@@ -3,34 +3,53 @@ import numpy as np
 from scipy import integrate
 from numpy import linspace
 
-
-
-def num_solve(max,c,m,g):
-    range=np.arange(0,max,0.005)
-    def eqn(Y, t):
-        v=((Y[0]**2)+(Y[1]**2))**0.5
-        eq =[(-1*c/m)*v*Y[0],-1*g-(c/m)*v*Y[1]] 
-        return eq
-    sol=integrate.odeint(eqn,[20,20],range)
+def dydt(Y, t,c,m,g ):
+    x,y,vx,vy = Y[0],Y[1],Y[2],Y[3]
+    v = np.sqrt(vx**2 + vy**2)
+    eq = [vx, vy, (-c/m) * v * vx, -g - (c/m) * v * vy]
+    return eq
+def num_solve(tmax,Y0,c,m,g):
+    t=np.linspace(0,tmax,1001)
+    sol=integrate.odeint(dydt,Y0,t, args=(c,m,g))
     return sol
-def solve_again(sol,max):
-    x=np.arange(0,max,0.005)
-    y1=sol[:,0]
-    y2=sol[:,1]
-    result1=integrate.cumtrapz(y1,x=x,initial=0)
-    result2=integrate.cumtrapz(y2,x=x,initial=0)
-    solution=[result1,result2]
-    return solution
-def plot(sol):
-    x=sol[0]
-    y=sol[1]
-    fig,ax=plt.subplots()
-    ax.plot(x,y)
-    fig.savefig('plot1.png')
+def xnormal(t,vx,x0):
+    x = vx*t+x0
+    return x
+
+def ynormal(t,vy,y0,g):
+    y = vy*t-(1/2)*g*t**2+y0
+    return y
+def plot_position(sol,tmax,g,y0,x0,vx,vy):
+    t = np.linspace(0, tmax, 1001)
+    x_normal = xnormal(t,vx,x0)
+    y_normal = ynormal(t,vy,y0,g)
+    x = sol[:, 0]
+    y = sol[:, 1]
+    fig, ax = plt.subplots()
+    ax.plot(x, y, label='Quadratic Drag')
+    ax.plot(x_normal, y_normal, label='No Drag')
+    plt.legend(loc='best')
+    ax.set_xlabel('X-position')
+    ax.set_ylabel('Y-position')
+    ax.set_title('Projectile Motion with Quadratic Drag - Position')
+    plt.savefig('plot_position.png')
+
+def plot_velocity(sol,tmax):
+    t = np.linspace(0, tmax, 1001)
+    vx = sol[:, 2]
+    vy = sol[:, 3]
+    fig, ax = plt.subplots()
+    ax.plot(t, vx, label='X-Velocity')
+    ax.plot(t, vy, label='Y-Velocity')
+    plt.legend(loc='best')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Velocity')
+    ax.set_title('Projectile Motion with Quadratic Drag - Velocity')
+    plt.savefig('plot_velocity.png')
+
 def main():
-    sol=num_solve(40,0.5,0.5,-9.8)
-    solution=solve_again(sol,40)
-    plot(solution)
+    sol = num_solve(10, [0, 0, 50, 50], 0.5, 10, 9.8)
+    plot_position(sol,10,9.8,0,0,50,50)
+    plot_velocity(sol,10)
+
 main()
-
-
