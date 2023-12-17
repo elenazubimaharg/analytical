714c04c74c5824d0f4eb8080a65b64c873a9326f
elenazubimaharg <145853111+elenazubimaharg@users.noreply.github.com>
2023-12-16 23:07:00 -0500

    Update analyticalproject.py

--- analyticalproject.py
+++ analyticalproject.py
@@ -26,7 +26,7 @@ def plot_position(sol,tmax,g,y0,x0,vx,vy):
     x = sol[:, 0]
     y = sol[:, 1]
     fig, ax = plt.subplots()
-    ax.plot(x, y, label='Quadratic Drag')
+    ax.plot(x, y, label='Quadratic Drag, C = 0.2')
     ax.plot(x_normal, y_normal, label='No Drag')
     plt.legend(loc='best')
     ax.set_xlabel('X-position')
@@ -34,13 +34,67 @@ def plot_position(sol,tmax,g,y0,x0,vx,vy):
     ax.set_title('Projectile Motion with Quadratic Drag - Position')
     plt.savefig('plot_position.png')
 
+def plot_position_c(tmax,g,y0,x0,vx,vy):
+    m=10
+    c=0.01
+    Y0=[0, 0, 20, 20]
+    xy_list=[]
+    for i in range(10):
+        sol = num_solve(tmax,Y0,c,m,g)
+        x_temp=sol[:,0]
+        y_temp=sol[:,1]
+        xy_temp=[x_temp,y_temp,c]
+        xy_list.append(xy_temp)
+        c+=0.005
+    t = np.linspace(0, tmax, 1001)
+    x_normal = xnormal(t,vx,x0)
+    y_normal = ynormal(t,vy,y0,g)
+    x_1 = xy_list[0][0]
+    y_1 = xy_list[0][1]
+    x_2 = xy_list[1][0]
+    y_2=xy_list[1][1]
+    x_3=xy_list[2][0]
+    y_3=xy_list[2][1]
+    x_4=xy_list[3][0]
+    y_4=xy_list[3][1]
+    x_5=xy_list[4][0]
+    y_5=xy_list[4][1]
+    x_6 = xy_list[5][0]
+    y_6 = xy_list[5][1]
+    x_7 = xy_list[6][0]
+    y_7 = xy_list[6][1]
+    x_8 = xy_list[7][0]
+    y_8 = xy_list[7][1]
+    x_9 = xy_list[8][0]
+    y_9 = xy_list[8][1]
+    x_10 = xy_list[9][0]
+    y_10 = xy_list[9][1]
+    fig, ax = plt.subplots()
+    ax.plot(x_normal, y_normal, label='No Drag')
+    ax.plot(x_1, y_1,label='C=0.01')
+    ax.plot(x_2, y_2,label='C=0.015')
+    ax.plot(x_3, y_3, label='C=0.02')
+    ax.plot(x_4, y_4, label='C=0.025')
+    ax.plot(x_5, y_5, label='C=0.03')
+    ax.plot(x_6, y_6, label='C=0.035')
+    ax.plot(x_7, y_7, label='C=0.04')
+    ax.plot(x_8, y_8, label='C=0.045')
+    ax.plot(x_9, y_9, label='C=0.05')
+    ax.plot(x_10, y_10, label='C=0.055')
+
+    plt.legend(loc='best')
+    ax.set_xlabel('X-position')
+    ax.set_ylabel('Y-position')
+    ax.set_title('Projectile Motion with Quadratic Drag - Position')
+    plt.savefig('plot_position_c.png')
+
 def plot_velocity(sol,tmax):
     t = np.linspace(0, tmax, 1001)
     vx = sol[:, 2]
     vy = sol[:, 3]
     fig, ax = plt.subplots()
-    ax.plot(t, vx, label='X-Velocity')
-    ax.plot(t, vy, label='Y-Velocity')
+    ax.plot(t, vx, label='X-Velocity (C = 0.2)')
+    ax.plot(t, vy, label='Y-Velocity (C = 0.2)')
     plt.legend(loc='best')
     ax.set_xlabel('Time')
     ax.set_ylabel('Velocity')
@@ -48,8 +102,9 @@ def plot_velocity(sol,tmax):
     plt.savefig('plot_velocity.png')
 
 def main():
-    sol = num_solve(10, [0, 0, 50, 50], 0.5, 10, 9.8)
-    plot_position(sol,10,9.8,0,0,50,50)
+    sol = num_solve(10, [0, 0, 20, 20], 0.2, 10, 9.8)
+    plot_position(sol,10,9.8,0,0,20,20)
+    plot_position_c(10, 9.8, 0, 0, 20, 20)
     plot_velocity(sol,10)
 
 main()
